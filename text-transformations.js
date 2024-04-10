c06965616508f4cd15510370bbc4bf9ec54c651a
cam465@pitt.edu <cam465@pitt.edu>
2024-04-10 19:41:26 -0400

    still troubleshooting. js does not work

--- text-transformations.js
+++ text-transformations.js
@@ -8,9 +8,10 @@ var hungarian
 var italian
 var latin
 
+
 /* INITIALIZION */
 function init() {
-    var buttons = document.getElementsByTagName("button")
+    var buttons = document.getElementsByName("button")
     slavic = document.getElementsByClassName("slavic")
     turkic = document.getElementsByClassName("turkic")
     germanic = document.getElementsByClassName("germanic")
@@ -18,46 +19,39 @@ function init() {
     hungarian = document.getElementsByClassName("hungarian")
     italian = document.getElementsByClassName("italian")
     latin = document.getElementsByClassName("latin")
+    console.log(buttons)
     /* SLAVIC ORIGIN BUTTONS */
-    var SlavItalicButton = buttons[3]
-    SlavItalicButton.addEventListener('click', Italic(slavic))
-    var SlavBoldButton = buttons[4]
-    SlavBoldButton.addEventListener('click', Slav_Bold)
-    var SlavUnderButton = buttons[5]
-    SlavUnderButton.addEventListener('click', Slav_Under)
-    var SlavClearButton = buttons[6]
-    SlavClearButton.addEventListener('click', Slav_Clear)
+    var SlavItalicButton = buttons[3];
+    SlavItalicButton.addEventListener('click', Italic(slavic));
+    var SlavBoldButton = buttons[4];
+    SlavBoldButton.addEventListener('click', Bold(slavic));
+    var SlavUnderButton = buttons[5];
+    SlavUnderButton.addEventListener('click', Under(slavic));
+    var SlavClearButton = buttons[6];
+    SlavClearButton.addEventListener('click', Clear(turkic));
     /* TURKIC ORIGIN BUTTONS */
-    var TurkItalicButton = buttons[7]
-    TurkItalicButton.addEventListener('click', Italic(turkic))
-    var TurkBoldButton = buttons[8]
-    TurkBoldButton.addEventListener('click', Turk_Bold)
-    var TurkUnderButton = buttons[9]
-    TurkUnderButton.addEventListener('click', Turk_Under)
-    var TurkClearButton = buttons[10]
-    TurkClearButton.addEventListener('click', Turk_Clear)
+    var TurkItalicButton = buttons[7];
+    TurkItalicButton.addEventListener('click', Italic(turkic));
+    var TurkBoldButton = buttons[8];
+    TurkBoldButton.addEventListener('click', Bold(turkic));
+    var TurkUnderButton = buttons[9];
+    TurkUnderButton.addEventListener('click', Under(turkic));
+    var TurkClearButton = buttons[10];
+    TurkClearButton.addEventListener('click', Clear(turkic));
     /* OTHER ORIGIN BUTTONS */
-    var OtherItalicButton = buttons[11]
-    OtherItalicButton.addEventListener('click', OtherItalic)
-    var OtherBoldButton = buttons[12]
-    OtherBoldButton.addEventListener('click', Other_Bold)
-    var OtherUnderButton = buttons[13]
-    OtherUnderButton.addEventListener('click', Other_Under)
-    var OtherClearButton = buttons[14]
-    OtherClearButton.addEventListener('click', Other_Clear)
+    var OtherItalicButton = buttons[11];
+    OtherItalicButton.addEventListener('click', OtherItalic);
+    var OtherBoldButton = buttons[12];
+    OtherBoldButton.addEventListener('click', Other_Bold);
+    var OtherUnderButton = buttons[13];
+    OtherUnderButton.addEventListener('click', Other_Under);
+    var OtherClearButton = buttons[14];
+    OtherClearButton.addEventListener('click', Other_Clear);
     /* ALPHABET BUTTONS */
-    var LatinButton = buttons[15]
-    LatinButton.addEventListener('click', Latin)
-    var CyrillicButton = buttons[16]
-    CyrillicButton.addEventListener('click', Cyrillic_Bold)
-}
-
-function OtherItalic() {
-    Italic(germanic)
-    Italic(greek)
-    Italic(hungarian)
-    Italic(italian)
-    Italic(latin)
+    var LatinButton = buttons[15];
+    LatinButton.addEventListener('click', Latin);
+    var CyrillicButton = buttons[16];
+    CyrillicButton.addEventListener('click', Cyrillic);
 }
 
 function Italic(input) {
@@ -68,67 +62,38 @@ function Italic(input) {
         };
 }
 
-
-/* SLAVIC FUNCTIONS */
-function Slav_Italic() {
-    var x = document.getElementsByClassName ("slavic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.fontStyle = "italic"
-    };
+function Bold(input) {
+    console.log("buttonPushed")
+    for(let i = 0; i < input.length; i++)
+        {
+        input[i].style.fontWeight = "bold"
+        };
 }
 
-function Slav_Bold() {
-    var x = document.getElementsByClassName ("slavic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.fontWeight = "bold"
-    };
+function Under(input) {
+    console.log("buttonPushed")
+    for(let i = 0; i < input.length; i++)
+        {
+        input[i].style.textDecoration = "underline"
+        };
 }
 
-function Slav_Under() {
-    var x = document.getElementsByClassName ("slavic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.textDecoration = "underline"
-    };
+function Clear(input) {
+    console.log("buttonPushed")
+    for(let i = 0; i < input.length; i++)
+        {
+        input[i].style.textDecoration = "none"
+        input[i].style.fontWeight = "normal"
+        input[i].style.fontStyle = "normal"
+        };
 }
 
-function Slav_Clear() {
-    var x = document.getElementsByClassName ("slavic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.textDecoration = "none"
-        x[i].style.fontWeight = "normal"
-        x[i].style.fontStyle = "normal"
-    };
-}
-
-/* TURKIC FUNCTIONS */
-function Turk_Italic() {
-    var x = document.getElementsByClassName ("turkic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.fontStyle = "italic"
-    };
-}
-
-function Turk_Bold() {
-    var x = document.getElementsByClassName ("turkic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.fontWeight = "bold"
-    };
-}
-
-function Turk_Under() {
-    var x = document.getElementsByClassName ("turkic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.textDecoration = "underline"
-    };
-}
-
-function Turk_Clear() {
-    var x = document.getElementsByClassName ("turkic");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.textDecoration = "none"
-        x[i].style.fontWeight = "normal"
-        x[i].style.fontStyle = "normal"
-    };
+function OtherItalic() {
+    Italic(germanic)
+    Italic(greek)
+    Italic(hungarian)
+    Italic(italian)
+    Italic(latin)
 }
 
 /* OTHER FUNCTIONS */
@@ -161,3 +126,5 @@ function Other_Clear() {
         x[i].style.fontStyle = "normal"
     };
 }
+
+window.onload = init();
