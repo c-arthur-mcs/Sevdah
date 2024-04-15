e57fdfb5bcde2ec17c53ab57f8323f2696616e7f
cam465@pitt.edu <cam465@pitt.edu>
2024-04-15 09:42:44 -0400

    add in functional js.

--- text-transformations.js
+++ text-transformations.js
@@ -1,100 +1,31 @@
 
-/* GLOBAL VARIABLES */
-var slavic
-var turkic
-var germanic
-var greek
-var hungarian
-var italian
-var latin
-
-
-/* INITIALIZION */
-function init() {
-    var buttons = document.getElementsByName("button")
-    slavic = document.getElementsByClassName("slavic")
-    turkic = document.getElementsByClassName("turkic")
-    germanic = document.getElementsByClassName("germanic")
-    greek = document.getElementsByClassName("greek")
-    hungarian = document.getElementsByClassName("hungarian")
-    italian = document.getElementsByClassName("italian")
-    latin = document.getElementsByClassName("latin")
-    /*console.log(buttons)*/
-    /* SLAVIC ORIGIN BUTTONS */
-    var SlavItalicButton = buttons[3];
-    SlavItalicButton.addEventListener('click', Italic(slavic));
-    var SlavBoldButton = buttons[4];
-    SlavBoldButton.addEventListener('click', Bold(slavic));
-    var SlavUnderButton = buttons[5];
-    SlavUnderButton.addEventListener('click', Under(slavic));
-    var SlavClearButton = buttons[6];
-    SlavClearButton.addEventListener('click', Clear(turkic));
-    /* TURKIC ORIGIN BUTTONS */
-    var TurkItalicButton = buttons[7];
-    TurkItalicButton.addEventListener('click', Italic(turkic));
-    var TurkBoldButton = buttons[8];
-    TurkBoldButton.addEventListener('click', Bold(turkic));
-    var TurkUnderButton = buttons[9];
-    TurkUnderButton.addEventListener('click', Under(turkic));
-    var TurkClearButton = buttons[10];
-    TurkClearButton.addEventListener('click', Clear(turkic));
-    /* OTHER ORIGIN BUTTONS */
-    var OtherItalicButton = buttons[11];
-    OtherItalicButton.addEventListener('click', OtherItalic);
-    var OtherBoldButton = buttons[12];
-    OtherBoldButton.addEventListener('click', Other_Bold);
-    var OtherUnderButton = buttons[13];
-    OtherUnderButton.addEventListener('click', Other_Under);
-    var OtherClearButton = buttons[14];
-    OtherClearButton.addEventListener('click', Other_Clear);
-    /* ALPHABET BUTTONS */
-    var LatinButton = buttons[15];
-    LatinButton.addEventListener('click', Latin);
-    var CyrillicButton = buttons[16];
-    CyrillicButton.addEventListener('click', Cyrillic);
+function textTransformationsInit() {
+  var languageToggles = document.getElementsByClassName("languageToggle")
+  for (var i = 0, len = languageToggles.length; i < len; i++) {
+    languageToggles[i].addEventListener("click", changeStyle, false);
+  }
 }
 
-function Italic(input) {
-    console.log("buttonPushed")
-    for(let i = 0; i < input.length; i++)
-        {
-        input[i].style.fontStyle = "italic"
-        };
+function changeStyle(e) {
+  console.log(this);
+  var lg = this.dataset.language;
+  var style = this.dataset.style;
+  if ([ 'slavic', 'turkic'].includes(lg)) {
+    var targets = document.getElementsByClassName(lg);
+  } else {
+    var targets = document.querySelectorAll(".germanic, .greek, .hungarian, .italian, .latin");
+  }
+  console.log(targets);
+  for (var i = 0, len = targets.length; i < len; i++) {
+    if (style == 'clear') {[ 'italic', 'bold', 'under'].forEach(function (j) {
+        targets[i].classList.remove(j);
+      })
+    } else {
+      targets[i].classList.add(style);
+    }
+  }
 }
 
-function Bold(input) {
-    console.log("buttonPushed")
-    for(let i = 0; i < input.length; i++)
-        {
-        input[i].style.fontWeight = "bold"
-        };
-}
-
-function Under(input) {
-    console.log("buttonPushed")
-    for(let i = 0; i < input.length; i++)
-        {
-        input[i].style.textDecoration = "underline"
-        };
-}
-
-function Clear(input) {
-    console.log("buttonPushed")
-    for(let i = 0; i < input.length; i++)
-        {
-        input[i].style.textDecoration = "none"
-        input[i].style.fontWeight = "normal"
-        input[i].style.fontStyle = "normal"
-        };
-}
-
-function OtherItalic() {
-    Italic(germanic)
-    Italic(greek)
-    Italic(hungarian)
-    Italic(italian)
-    Italic(latin)
-}
 
 function Latin() {
     var x = document.getElementsByClassName("cyrillic");
@@ -102,53 +33,24 @@ function Latin() {
         {
         x[i].style.display = "none"
         };  
-    var y = document.getElementsByClassName("lat");
+    var y = document.getElementsByClassName("latin");
     for(let i = 0; i < y.length; i++)
         {
-        y[i].style.display = "none"
+        y[i].style.display = "block"
         };  
-        
+}
+
 function Cyrillic() {
-    var x = document.getElementsByClassName("lat");
+    var x = document.getElementsByClassName("latin");
     for(let i = 0; i < x.length; i++)
         {
         x[i].style.display = "none"
         };  
-    var y = document.getElementsByClassName("cyr");
+    var y = document.getElementsByClassName("cyrillic");
     for(let i = 0; i < y.length; i++)
         {
-        y[i].style.display = "none"
+        y[i].style.display = "block"
         };  
-
-/* OTHER FUNCTIONS */
-function Other_Italic() {
-    var x = document.getElementsByClassName ("germanic", "greek", "hungarian", "italian", "latin");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.fontStyle = "italic"
-    };
 }
 
-function Other_Bold() {
-    var x = document.getElementsByClassName ("germanic", "greek", "hungarian", "italian", "latin");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.fontWeight = "bold"
-    };
-}
-
-function Other_Under() {
-    var x = document.getElementsByClassName ("germanic", "greek", "hungarian", "italian", "latin");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.textDecoration = "underline"
-    };
-}
-
-function Other_Clear() {
-    var x = document.getElementsByClassName ("germanic", "greek", "hungarian", "italian", "latin");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.textDecoration = "none"
-        x[i].style.fontWeight = "normal"
-        x[i].style.fontStyle = "normal"
-    };
-}
-/*
-window.onload = init();*/
+document.addEventListener('DOMContentLoaded', textTransformationsInit);
