eeacf099856fcab5e29f7dbbb903e79a06915ed6
cam465@pitt.edu <cam465@pitt.edu>
2024-04-23 13:49:33 -0400

    mockup for graphs_by_author.xhtml and creation of graphs_by_author.js

---
+++ graphs_by_author_buttons.js
@@ -0,0 +1,272 @@
+/* ALEKSA ŠANTIĆ */
+function Aleksa_Šantić() {
+    var a = document.getElementsByClassName("Aleksa_Šantić");
+    for(let i = 0; i < a.length; i++)
+        {
+        a[i].style.display = "block"
+        };  
+    var b = document.getElementsByClassName("Himzo_Polovina");
+    for(let i = 0; i < b.length; i++)
+        {
+        b[i].style.display = "none"
+        };  
+    var c = document.getElementsByClassName("Musa_Ćazim_Ćatić");
+    for(let i = 0; i < c.length; i++)
+        {
+        c[i].style.display = "none"
+        };  
+    var d = document.getElementsByClassName("Mustafa_Mujezinović");
+    for(let i = 0; i < d.length; i++)
+        {
+        d[i].style.display = "none"
+        };  
+    var e = document.getElementsByClassName("Osman_Đikić");
+    for(let i = 0; i < e.length; i++)
+        {
+        e[i].style.display = "none"
+        };  
+    var f = document.getElementsByClassName("Safetbeg_Bašagić");
+    for(let i = 0; i < f.length; i++)
+        {
+        f[i].style.display = "none"
+        };
+    var g = document.getElementsByClassName("Silvana_Armenulić");
+    for(let i = 0; i < g.length; i++)
+        {
+        g[i].style.display = "none"
+        };  
+}
+
+/* HIMZO POLOVINA */
+function Himzo_Polovina() {
+    var a = document.getElementsByClassName("Aleksa_Šantić");
+    for(let i = 0; i < a.length; i++)
+        {
+        a[i].style.display = "none"
+        };  
+    var b = document.getElementsByClassName("Himzo_Polovina");
+    for(let i = 0; i < b.length; i++)
+        {
+        b[i].style.display = "block"
+        };  
+    var c = document.getElementsByClassName("Musa_Ćazim_Ćatić");
+    for(let i = 0; i < c.length; i++)
+        {
+        c[i].style.display = "none"
+        };  
+    var d = document.getElementsByClassName("Mustafa_Mujezinović");
+    for(let i = 0; i < d.length; i++)
+        {
+        d[i].style.display = "none"
+        };  
+    var e = document.getElementsByClassName("Osman_Đikić");
+    for(let i = 0; i < e.length; i++)
+        {
+        e[i].style.display = "none"
+        };  
+    var f = document.getElementsByClassName("Safetbeg_Bašagić");
+    for(let i = 0; i < f.length; i++)
+        {
+        f[i].style.display = "none"
+        };
+    var g = document.getElementsByClassName("Silvana_Armenulić");
+    for(let i = 0; i < g.length; i++)
+        {
+        g[i].style.display = "none"
+        };  
+}
+
+/* MUSA ĆAZIM ĆATIĆ */
+function Musa_Ćazim_Ćatić() {
+    var a = document.getElementsByClassName("Aleksa_Šantić");
+    for(let i = 0; i < a.length; i++)
+        {
+        a[i].style.display = "none"
+        };  
+    var b = document.getElementsByClassName("Himzo_Polovina");
+    for(let i = 0; i < b.length; i++)
+        {
+        b[i].style.display = "none"
+        };  
+    var c = document.getElementsByClassName("Musa_Ćazim_Ćatić");
+    for(let i = 0; i < c.length; i++)
+        {
+        c[i].style.display = "block"
+        };  
+    var d = document.getElementsByClassName("Mustafa_Mujezinović");
+    for(let i = 0; i < d.length; i++)
+        {
+        d[i].style.display = "none"
+        };  
+    var e = document.getElementsByClassName("Osman_Đikić");
+    for(let i = 0; i < e.length; i++)
+        {
+        e[i].style.display = "none"
+        };  
+    var f = document.getElementsByClassName("Safetbeg_Bašagić");
+    for(let i = 0; i < f.length; i++)
+        {
+        f[i].style.display = "none"
+        };
+    var g = document.getElementsByClassName("Silvana_Armenulić");
+    for(let i = 0; i < g.length; i++)
+        {
+        g[i].style.display = "none"
+        };  
+}
+
+/* MUSTAFA MUJEZINOVIĆ */
+function Mustafa_Mujezinović() {
+    var a = document.getElementsByClassName("Aleksa_Šantić");
+    for(let i = 0; i < a.length; i++)
+        {
+        a[i].style.display = "none"
+        };  
+    var b = document.getElementsByClassName("Himzo_Polovina");
+    for(let i = 0; i < b.length; i++)
+        {
+        b[i].style.display = "none"
+        };  
+    var c = document.getElementsByClassName("Musa_Ćazim_Ćatić");
+    for(let i = 0; i < c.length; i++)
+        {
+        c[i].style.display = "none"
+        };  
+    var d = document.getElementsByClassName("Mustafa_Mujezinović");
+    for(let i = 0; i < d.length; i++)
+        {
+        d[i].style.display = "block"
+        };  
+    var e = document.getElementsByClassName("Osman_Đikić");
+    for(let i = 0; i < e.length; i++)
+        {
+        e[i].style.display = "none"
+        };  
+    var f = document.getElementsByClassName("Safetbeg_Bašagić");
+    for(let i = 0; i < f.length; i++)
+        {
+        f[i].style.display = "none"
+        };
+    var g = document.getElementsByClassName("Silvana_Armenulić");
+    for(let i = 0; i < g.length; i++)
+        {
+        g[i].style.display = "none"
+        };  
+}
+
+/* OSMAN ĐIKIĆ */
+function Osman_Đikić() {
+    var a = document.getElementsByClassName("Aleksa_Šantić");
+    for(let i = 0; i < a.length; i++)
+        {
+        a[i].style.display = "none"
+        };  
+    var b = document.getElementsByClassName("Himzo_Polovina");
+    for(let i = 0; i < b.length; i++)
+        {
+        b[i].style.display = "none"
+        };  
+    var c = document.getElementsByClassName("Musa_Ćazim_Ćatić");
+    for(let i = 0; i < c.length; i++)
+        {
+        c[i].style.display = "none"
+        };  
+    var d = document.getElementsByClassName("Mustafa_Mujezinović");
+    for(let i = 0; i < d.length; i++)
+        {
+        d[i].style.display = "none"
+        };  
+    var e = document.getElementsByClassName("Osman_Đikić");
+    for(let i = 0; i < e.length; i++)
+        {
+        e[i].style.display = "block"
+        };  
+    var f = document.getElementsByClassName("Safetbeg_Bašagić");
+    for(let i = 0; i < f.length; i++)
+        {
+        f[i].style.display = "none"
+        };
+    var g = document.getElementsByClassName("Silvana_Armenulić");
+    for(let i = 0; i < g.length; i++)
+        {
+        g[i].style.display = "none"
+        };  
+}
+
+/* SAFET-BEG BAŠAGIĆ */
+function Safetbeg_Bašagić() {
+    var a = document.getElementsByClassName("Aleksa_Šantić");
+    for(let i = 0; i < a.length; i++)
+        {
+        a[i].style.display = "none"
+        };  
+    var b = document.getElementsByClassName("Himzo_Polovina");
+    for(let i = 0; i < b.length; i++)
+        {
+        b[i].style.display = "none"
+        };  
+    var c = document.getElementsByClassName("Musa_Ćazim_Ćatić");
+    for(let i = 0; i < c.length; i++)
+        {
+        c[i].style.display = "none"
+        };  
+    var d = document.getElementsByClassName("Mustafa_Mujezinović");
+    for(let i = 0; i < d.length; i++)
+        {
+        d[i].style.display = "none"
+        };  
+    var e = document.getElementsByClassName("Osman_Đikić");
+    for(let i = 0; i < e.length; i++)
+        {
+        e[i].style.display = "none"
+        };  
+    var f = document.getElementsByClassName("Safetbeg_Bašagić");
+    for(let i = 0; i < f.length; i++)
+        {
+        f[i].style.display = "block"
+        };
+    var g = document.getElementsByClassName("Silvana_Armenulić");
+    for(let i = 0; i < g.length; i++)
+        {
+        g[i].style.display = "none"
+        };  
+}
+
+/* SILVANA ARMENULIĆ */
+function Silvana_Armenulić() {
+    var a = document.getElementsByClassName("Aleksa_Šantić");
+    for(let i = 0; i < a.length; i++)
+        {
+        a[i].style.display = "none"
+        };  
+    var b = document.getElementsByClassName("Himzo_Polovina");
+    for(let i = 0; i < b.length; i++)
+        {
+        b[i].style.display = "none"
+        };  
+    var c = document.getElementsByClassName("Musa_Ćazim_Ćatić");
+    for(let i = 0; i < c.length; i++)
+        {
+        c[i].style.display = "none"
+        };  
+    var d = document.getElementsByClassName("Mustafa_Mujezinović");
+    for(let i = 0; i < d.length; i++)
+        {
+        d[i].style.display = "none"
+        };  
+    var e = document.getElementsByClassName("Osman_Đikić");
+    for(let i = 0; i < e.length; i++)
+        {
+        e[i].style.display = "none"
+        };  
+    var f = document.getElementsByClassName("Safetbeg_Bašagić");
+    for(let i = 0; i < f.length; i++)
+        {
+        f[i].style.display = "none"
+        };
+    var g = document.getElementsByClassName("Silvana_Armenulić");
+    for(let i = 0; i < g.length; i++)
+        {
+        g[i].style.display = "block"
+        };  
+}
